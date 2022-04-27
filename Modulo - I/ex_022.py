73d6090d7ac1cbd598257300d50cfd22d60b1c83
Mussa <nelsonluismussa@gmail.com>
2022-04-27 19:55:01 +0200

    basico

--- Modulo - I/ex_022.py
+++ Modulo - I/ex_022.py
@@ -3,8 +3,9 @@
 Conforme a letra escrever: F - Feminino, M - Masculino, Sexo Inválido. 
 '''
 
+
 digitada = str(input('Verifique se uma letra digitada é "F" ou "M":'))
 if digitada in 'Ff' or digitada in 'Mm' or digitada == 'Masculino' or digitada == 'Feminino':
-    print(f'{digitada}')
+    print(f'O Seu Sexo e : {digitada}.')
 else:
     print('Sexo Inválido')
